25e3498ae08a7d11ec8fd8752ecad90251c44ff8
Helen <rutehelenrh@gmail.com>
2022-05-04 20:28:27 -0300

    Adicionando projeto back-end Farmacia

---
+++ farmaciasaude/src/main/java/com/farmacia/farmaciasaude/FarmaciasaudeApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmaciasaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciasaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciasaudeApplication.class, args);
+	}
+
+}
---
+++ farmaciasaude/src/test/java/com/farmacia/farmaciasaude/FarmaciasaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmaciasaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciasaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
